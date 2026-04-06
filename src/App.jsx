0a600aff9c23ab9d067825d198cde03a207133df
luckybamhore-glitch <luckybamhore@gmail.com>
2026-04-06 12:33:18 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,65 @@
-import React, { useState } from 'react'
+ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Form from './components/Form'
-import ListGroup from './components/ListGroup'
+import ListGroup from './components/ListGroup.jsx'
 
 const App = () => {
 
-const [todos, setTodos] = useState([{
- id: 1,
- text: "Sample Text"
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Sample Text"
+    }
+  ])
 
-}])
-
-
-const [edit , setEdit] = useState(
-  {
+  const [edit, setEdit] = useState({
     todo: {},
     isEdit: false
-  }
-)
-
-const addTodo = (newTodo) => {
-   
-  setTodos([newTodo, ...todos])
-
-}
-
-const removeTodo = (id) =>{
-  setTodos(todos.filter(todo => todo.id !== id))
-}
-
-const editTodo = (todo) => {
-  setEdit({
-    todo: todo,
-    isEdit: true
   })
-}
- 
-const updateTodo =(updatedTodo) =>{
-  setTodos(todos.map(todo => todo.id===updatedTodo.id ? updatedTodo:todo))
-  setEdit({todo: {}, isEdit: false})
-}
+
+  const addTodo = (newTodo) => {
+    setTodos([newTodo, ...todos])
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  const editTodo = (todo) => {
+    setEdit({
+      todo: todo,
+      isEdit: true
+    })
+  }
+
+  const updateTodo = (updatedTodo) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === updatedTodo.id ? updatedTodo : todo
+      )
+    )
+    setEdit({ todo: {}, isEdit: false })
+  }
 
   return (
     <>
-  <Navbar/> 
-    
-    <div className='p-8'>
-      <Form addTodo = {addTodo} edit={edit} updateTodo={updateTodo}/>
-      <ListGroup todos={todos} removeTodo ={removeTodo} editTodo ={editTodo}/>
-     
-    
-    </div>
+      <Navbar />
 
+      <div className='p-8'>
+        <Form
+          addTodo={addTodo}
+          edit={edit}
+          updateTodo={updateTodo}
+        />
 
+        <ListGroup
+          todos={todos}
+          removeTodo={removeTodo}
+          editTodo={editTodo}
+        />
+      </div>
     </>
   )
 }
 
-export default App;
+export default App
